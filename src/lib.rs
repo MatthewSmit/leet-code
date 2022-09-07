1d6d692adc7b37eac6f347aee296ca802153607e
Matthew Smit <matthew.smit@scopesolutions.co.nz>
2022-09-07 13:10:26 +1200

    Implementation of 383

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,29 @@
+use std::collections::HashMap;
+
 pub struct Solution{}
 
 impl Solution {
     pub fn can_construct(ransom_note: String, magazine: String) -> bool {
-        unimplemented!()
+        let mut letters = HashMap::new();
+
+        for c in magazine.chars() {
+            letters.entry(c)
+                .and_modify(|v| *v += 1)
+                .or_insert(1);
+        }
+
+        let mut zero = 0;
+
+        for c in ransom_note.chars() {
+            let letter = letters.get_mut(&c).or_else(|| Option::from(&mut zero)).unwrap();
+            if *letter > 0 {
+                *letter -= 1
+            } else {
+                return false
+            }
+        }
+
+        true
     }
 }
 
